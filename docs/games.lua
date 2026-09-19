2778512b37b7de421d2939a238e14f2b56edb4cd
fischisreal <finngleit@gmail.com>
2026-09-19 15:47:22 +0200

    Update games.lua

--- docs/games.lua
+++ docs/games.lua
@@ -1,1 +1,13 @@
-local Message = "Hey! A script for this game already exists!"
+{
+    GameID = 1234567890,
+    UniverseID = 987654321,
+    CreatorID = 111222333,
+    GameName = "Example Game",
+    NameContains = {"Example Game", "Example"},
+    RequiredPaths = {
+        "workspace.ExampleFolder",
+        "ReplicatedStorage.ExampleRemote"
+    },
+    Script = "https://example.com/example.lua",
+    ScriptName = "Example Game Script",
+},
